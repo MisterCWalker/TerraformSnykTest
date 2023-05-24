5cd3f174490d46a5a09075a8bd4ceec453d2c27d
Craig Walker <craig.walker@methods.co.uk>
2023-05-24 17:30:13 +0100

    Lambs

---
+++ api-gateway.tf
@@ -0,0 +1,39 @@
+resource "aws_apigatewayv2_api" "myhttpapi" {
+  name          = "my-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.myhttpapi.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_integration" "my_lambda" {
+  api_id           = aws_apigatewayv2_api.myhttpapi.id
+  integration_type = "AWS_PROXY"
+
+  integration_uri = aws_lambda_function.my_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "example" {
+  api_id    = aws_apigatewayv2_api.myhttpapi.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.my_lambda.id}"
+}
+
+resource "aws_apigatewayv2_deployment" "example" {
+  api_id      = aws_apigatewayv2_api.myhttpapi.id
+  description = "My deployment"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_apigatewayv2_route.example
+  ]
+}
---
+++ lambda.tf
@@ -0,0 +1,8 @@
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my_lambda"
+  handler       = "index.handler"
+  role          = aws_iam_role.iam_for_lambda.arn
+  runtime       = "nodejs14.x"
+
+  filename = "lambda/lambda-package.zip"
+}
