882209cf5c0d2d3212ed755e43756cce0b9fcfbf
Craig Walker <craig.walker@methods.co.uk>
2023-06-13 17:43:50 +0100

    Fixes

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.0"
+    }
   }
   backend "s3" {}
 }
