28e13b8919dd48c15cfe9eff48974d56738e26e6
Craig Walker <craig.walker@methods.co.uk>
2023-06-14 09:30:05 +0100

    ASD

--- api-gateway.tf
+++ api-gateway.tf
@@ -85,6 +85,20 @@ resource "aws_api_gateway_base_path_mapping" "mapping" {
   domain_name = aws_api_gateway_domain_name.domain.domain_name
 }
 
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+}
+
 ######
 # ACM
 ######
